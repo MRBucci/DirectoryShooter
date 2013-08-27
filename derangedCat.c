76b714dcf6a9348dbc2ded9b5e092ff6ae8b772c
adkscherch <adkscherch@gmail.com>
2013-08-27 00:34:37 -0400

    Add comments, remove lines.

--- derangedCat.c
+++ derangedCat.c
@@ -16,30 +16,30 @@ int main(int argc, char *argv[])
 {
 	char dirA[] = "./";
 	strcat(dirA,".");
-	int* arraySize = malloc(sizeof(int));
-	int* fileVals = checkDirs(dirA, arraySize);
-	int i;
+	int* arraySize = malloc(sizeof(int));//The arraySize variable is initialized solely to circumvent
+	int* fileVals = checkDirs(dirA, arraySize);//the issue that the program can't normally determine
+	int i;                                //the size of an array at runtime if it's dynamically allocated.
 	FILE *fp;
-	fp = fopen("layout.txt", "w");
-	for (i = 0; i < *arraySize; i++)
+	fp = fopen("layout.txt", "w");  //The integer array is fed into this file.
+	for (i = 0; i < *arraySize; i++)//arraySize's only use is here.
 		fprintf(fp,"%d\n",fileVals[i]);
 	free(arraySize);
+	fclose(fp);
 	free(fileVals);
 	return 0;
 }
 
-//This method is the meat of the program, taking in two paths and searching their directories.
-//This is the first time I can actually feel really thankful for recursion.
+
 int* checkDirs(char* apath, int* arraySize){
 	DIR *a;
         struct dirent *adent; 
 	struct stat *dirA;
 	dirA = malloc(sizeof(stat));
 	char **Anames;
-	char currentFile[128];//get the job done for small files and I wasn't given a size limit.
+	char currentFile[128];//128 is more than enough characters for a file title.
 	Anames = malloc(sizeof(char*));
 	strcat(apath,"/");
-	char tempA[256];
+	char tempA[256];  //used in the file scanning process.
 	int aItems = 0;
         char* aString;
         if (!(a = opendir(apath))){				//check if both directories can open first.
@@ -70,8 +70,6 @@ int* checkDirs(char* apath, int* arraySize){
 	//Search through A!
 	for (i = 0; i < aItems; i++){
 		finalVals[i] = wordSort(Anames[i]);		//Set the final int based on the file extension!
-		strcpy(tempA,apath);
-		strcat(tempA,Anames[i]);
         }
 	free(dirA);
 	free(Anames);
@@ -79,6 +77,8 @@ int* checkDirs(char* apath, int* arraySize){
 	return finalVals;
 }
 
+//This method takes in a string, uses strstr to try to find a file extension,
+//and returns an integer based on the existence of such an extension.
 int wordSort(char* fileNames){
 	int i;
 	char* per; 
