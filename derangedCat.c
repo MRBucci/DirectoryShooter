dc138ee10702e595f9975db80dd2e7fb6c062ca7
adkscherch <adkscherch@gmail.com>
2013-08-26 21:39:55 -0400

    Why is it so messed up

--- derangedCat.c
+++ derangedCat.c
@@ -1,8 +1,5 @@
-//The Equalizer, by Andrew Scherch, for CS 283 Lab 3
-//This program essentially functions by recursively running
-//the checkDirs method which searches and "equalizes" directory
-//B with directory A, as input by the user. Whatever is in A but not
-//in B is copied over, while whatever is in B but not A is deleted.
+//Put this program in a Linux directory, and it will take all of the files
+//and convert them into readable data for the windows program to create a level with.
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -13,14 +10,12 @@
 
 int* checkDirs(char* apath, int* arraySize);
 
-int multiHash(char* aWord);
-
 int wordSort(char* fileNames);
 
 int main(int argc, char *argv[])
 {
 	char dirA[] = "./";
-	strcat(dirA,argv[1]);
+	strcat(dirA,".");
 	int* arraySize = malloc(sizeof(int));
 	int* fileVals = checkDirs(dirA, arraySize);
 	int i;
@@ -41,7 +36,6 @@ int* checkDirs(char* apath, int* arraySize){
 	struct stat *dirA;
 	dirA = malloc(sizeof(stat));
 	char **Anames;
-	char buffer[1024]; //This isn't a very large buffer, but it should
 	char currentFile[128];//get the job done for small files and I wasn't given a size limit.
 	Anames = malloc(sizeof(char*));
 	strcat(apath,"/");
@@ -52,7 +46,7 @@ int* checkDirs(char* apath, int* arraySize){
                 error("Failed to open directory a!");
 		exit(1);
 	}
-	while(0 != (adent = readdir(a))){ //Read through A's files and store their names and stats.
+	while(0 != (adent = readdir(a))){ //Read through A's files and store their names.
 		strcpy(currentFile,adent->d_name);
                 if (((strcmp(currentFile,".")) != 0) &&
                          ((strcmp(currentFile,"..")) != 0)){
@@ -70,32 +64,14 @@ int* checkDirs(char* apath, int* arraySize){
          	        }
 		}
         }
-	int i,dirCount,fileCount,nbytes,found,readFile,writeFile;
-	dirCount = 0;
+	int i;
 	*arraySize = aItems;
 	int* finalVals = malloc(((int)aItems) * sizeof(int));
-	fileCount = 0;
-	printf("aItems is %d", aItems);
-	//First, we search through A to find things that aren't in B to copy over, including directories.
+	//Search through A!
 	for (i = 0; i < aItems; i++){
-		finalVals[i] = wordSort(Anames[i]);				//found will be used with multiple checks of the directories.
+		finalVals[i] = wordSort(Anames[i]);		//Set the final int based on the file extension!
 		strcpy(tempA,apath);
 		strcat(tempA,Anames[i]);
-		/*if (S_ISDIR(dirA[i].st_mode) != 0){			//First, check if the current file is a directory.
-			returnFiles[0] = realloc(returnFiles[0],(dirCount+1) * sizeof(char*));
-			returnFiles[0][dirCount] = malloc((strlen(Anames[i])+1)*sizeof(char));
-			strcpy(returnFiles[0][dirCount],Anames[i]);
-			printf("%s",returnFiles[0][dirCount]);
-			dirCount = dirCount + 1;	
-			printf("Dircount %d %d \n",dirCount,sizeof(returnFiles[0]));
-			}
-		else{
-			returnFiles[1] = realloc(returnFiles[1],(fileCount+1) * sizeof(char*));
-			returnFiles[1][fileCount] = malloc((strlen(Anames[i])+1)*sizeof(char));
-                        strcpy(returnFiles[1][fileCount],Anames[i]);
-                        fileCount = fileCount + 1;
-			printf("Filecount %d %d \n",fileCount,sizeof(returnFiles[1]));
-		}*/
         }
 	free(dirA);
 	free(Anames);
@@ -103,22 +79,10 @@ int* checkDirs(char* apath, int* arraySize){
 	return finalVals;
 }
 
-
-int multiHash(char* aWord)
-{
-        int i;
-	int value = 0;
-	for (i = 0; i < strlen(aWord); i++){
-		value = value + aWord[i];
-	}
-	return value % 26;
-}
-
 int wordSort(char* fileNames){
 	int i;
 	char* per; 
 	per = strstr(fileNames,".");	
-	printf("per is %s\n", per);
 	if (per != NULL){
 		if ((strcmp(per, ".doc") == 0) || (strcmp(per, ".txt") == 0))
 			return 1;
